5b39dae60882191c6a49a91f2f0ded13f0af121d
AK-uni-git <55245797+AK-uni-git@users.noreply.github.com>
2019-11-16 13:17:26 +0200

    test7

--- script.py
+++ script.py
@@ -4,7 +4,8 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb+srv://spend:wise@spendwisedata-gaqmj.mongodb.net/Datas')
 print(client.database_names())
-db = client.Datas
+db = client['Datas']
+print(db.collection_names())
 print(db)
 collection = db.DataModels
 print(collection)
