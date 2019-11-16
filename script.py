3ee4856993acc05916336f011b60043da475620f
AK-uni-git <55245797+AK-uni-git@users.noreply.github.com>
2019-11-16 13:06:09 +0200

    test4

--- script.py
+++ script.py
@@ -5,7 +5,9 @@ client = MongoClient('mongodb+srv://spend:wise@spendwisedata-gaqmj.mongodb.net/D
 
 
 db = client.SpendWise
+print(db)
 collection = db.Datas
+print(collection)
 for post in collection.find():
         print(post)
 client.close()
