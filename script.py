7e775630cd846c24d208f31d83ca02964394bb15
AK-uni-git <aku.kauria@student.lut.fi>
2019-11-16 16:42:19 +0200

    test

--- script.py
+++ script.py
@@ -1,5 +1,13 @@
 import sys
 from pymongo import MongoClient
+import datetime
+
+class Person:
+  def __init__(self, name, purchaseDate, price, usage):
+    self.name = name
+    self.purchaseDate = purchaseDate
+    self.price = price
+    self.usage = usage
 
 if (len(sys.argv) > 1): 
         username = sys.argv[1]
@@ -13,8 +21,7 @@ print(db.collection_names())
 
 collection = db.buymodels
 
-for a in db.datamodels.find():
-        print(a)
+
 for user in collection.find( {'username': '{0}'.format(username)} ):
         print(int(user["ean"]) )
         for product in db.datamodels.find( {'ean': user["ean"] } ):
