78647b1ee111d7190d97ebb6a792f1a809841c4a
AK-uni-git <55245797+AK-uni-git@users.noreply.github.com>
2019-11-16 16:19:11 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -13,6 +13,8 @@ print(db.collection_names())
 
 collection = db.buymodels
 
+for a in db.datamodels.find():
+        print(a)
 for user in collection.find( {'username': '{0}'.format(username)} ):
         print(int(user["ean"]) )
         for product in db.datamodels.find( {'ean': '{0}'.format(int(user["ean"]) ) } ):
